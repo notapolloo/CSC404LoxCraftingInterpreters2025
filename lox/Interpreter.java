38a02a898fca04ea190498589f78c0db968c3be1
notapolloo <madison.howard5323@gmail.com>
2025-09-15 21:31:54 -0400

    Create Interpreter.java

---
+++ lox/Interpreter.java
@@ -0,0 +1,89 @@
+// Remove or update the package declaration if the file is not in a 'lox' directory.
+// If your file is not inside a 'lox' folder, remove the package declaration:
+package lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+
+      @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+
+  @Override
+  public Object visitUnaryExpr(Expr.Unary expr) {
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case GREATER:
+        return (double)left > (double)right;
+      case GREATER_EQUAL:
+        return (double)left >= (double)right;
+      case LESS:
+        return (double)left < (double)right;
+      case LESS_EQUAL:
+        return (double)left <= (double)right;
+      case BANG:
+        return !isTruthy(right);  
+      case MINUS:
+        return -(double)right;
+      case PLUS:
+        if (left instanceof Double && right instanceof Double) {
+          return (double)left + (double)right;
+        } 
+
+        if (left instanceof String && right instanceof String) {
+          return (String)left + (String)right;
+        }
+
+
+        break;
+      case BANG_EQUAL: return !isEqual(left, right);
+      case EQUAL_EQUAL: return isEqual(left, right);
+    }
+
+    // Unreachable.
+    return null;
+  }
+
+    private boolean isTruthy(Object object) {
+    if (object == null) return false;
+    if (object instanceof Boolean) return (boolean)object;
+    return true;
+  }
+
+private boolean isEqual(Object a, Object b) {
+    if (a == null && b == null) return true;
+    if (a == null) return false;
+
+    return a.equals(b);
+  }
+
+@Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
+
+    @Override
+  public Object visitBinaryExpr(Expr.Binary expr) {
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right); 
+
+    switch (expr.operator.type) {
+      case MINUS:
+        return (double)left - (double)right;
+      case SLASH:
+        return (double)left / (double)right;
+      case STAR:
+        return (double)left * (double)right;
+    }
+
+    // Unreachable.
+    return null;
+  }
+
+
+}
