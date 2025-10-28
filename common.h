a509f36794dce6c121ed3671cf4545b309fcbf4a
notapolloo <madison.howard5323@gmail.com>
2025-10-28 10:38:23 -0400

    Added 18

---
+++ common.h
@@ -0,0 +1,13 @@
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
+#define DEBUG_TRACE_FUNCTIONS
+
+
+#endif
